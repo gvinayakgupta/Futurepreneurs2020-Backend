586a683f7f84e9de4e17f0473e4f123c0eefbb28
gvinayakgupta <guptavinayakg@gmail.com>
2020-10-17 12:50:56 +0530

    cors

--- app.js
+++ app.js
@@ -36,18 +36,18 @@ app.use(bodyParser.json());
 
 
 // Allow CORS
-// app.use((req, res, next) => {
-// 	res.header("Access-Control-Allow-Origin", "*");
-// 	res.header(
-// 		"Access-Control-Allow-Headers",
-// 		"Origin, X-Requested-With, Content-Type, Accept, Authorization,auth-token"
-// 	);
-// 	if (req.method === "OPTIONS") {
-// 		res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-// 		return res.status(200).json({});
-// 	}
-// 	next();
-// });
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization,auth-token"
+	);
+	if (req.method === "OPTIONS") {
+		res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+		return res.status(200).json({});
+	}
+	next();
+});
 
 app.use(cors());
 // /////Rate Limiter
